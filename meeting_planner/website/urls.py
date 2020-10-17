ae677393a8d4e323b320233fdbe3a59d6ce34fd9
Luis Calderon <luis.calderon@anahuac.mx>
2020-10-17 13:37:28 -0500

    change of url patterns

--- meeting_planner/website/urls.py
+++ meeting_planner/website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
-    path('', views.welcome, name='welcome')
+    path('', views.welcome, name='welcome'),
+    path('date', views.date, name='date'),
     ]
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to meeting planner website!')
+    return HttpResponse('Welcome to Meeting Planner website!')
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
