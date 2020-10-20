707da35ad0b15e184dc255dbb74e6bd8804739f4
Luis Calderon <luis.calderon@anahuac.mx>
2020-10-19 20:29:04 -0500

    add of welcome template html

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Meeting Planner website!')
+    return render(request, "website/welcome.html",
+                  {"message": "This data was sent from the view to the template."})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
