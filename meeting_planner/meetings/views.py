c831d329c62fbf3016dd5dd8a5c090b378811df0
Luis Calderon <luis.calderon@anahuac.mx>
2020-10-23 17:15:51 -0500

    change in htmk

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Meeting
+from .models import Meeting, Room
 
 # Create your views here.
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
+
+# Please add a nre page that shows a list of all room objects
+def rooms(request):
+    rooms = Room.objects.all()
+    return render(request, "meetings/room.html", rooms)
